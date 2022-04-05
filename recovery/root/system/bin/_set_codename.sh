664931c56c5d95f315e54b67525b2b7892d42fce
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-05 21:20:55 +0800

    mi439: Add scripts for users to change device codename easily
    [Jprimero15: add mi439 and remove omni]
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

---
+++ recovery/root/system/bin/_set_codename.sh
@@ -0,0 +1,9 @@
+#!/sbin/sh
+
+set_device_codename() {
+	resetprop "ro.build.product" "$1"
+	resetprop "ro.product.device" "$1"
+	for i in odm product system system_ext vendor; do
+		resetprop "ro.product.${i}.device" "$1"
+	done
+}
