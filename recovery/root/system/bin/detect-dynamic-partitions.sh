3680ced06e5a35f9efffe48386ca0e88ebdd107f
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-18 20:42:37 +0800

    mi439: Handle non dynamic partitions
    [adjust]
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>
    
     Conflicts:
    	recovery/root/init.recovery.qcom.rc
    	recovery/root/system/etc/recovery.fstab
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

---
+++ recovery/root/system/bin/detect-dynamic-partitions.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+
+if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep mi439_dynpart > /dev/null; then
+    # Dynamic partitions
+    cat /system/etc/recovery.fstab.dynamic >> /system/etc/recovery.fstab
+else
+    # Non Dynamic partitions
+    cat /system/etc/twrp.flags.nondynamic >> /system/etc/twrp.flags
+fi
