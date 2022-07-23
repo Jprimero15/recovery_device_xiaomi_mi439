4c94ff4cb856674a816aa45f148075816abdcee8
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-23 21:56:00 +0800

    mi439: Set display name for System_EXT partition
    [i prefer System_Ext]
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- recovery/root/system/bin/detect-dynamic-partitions.sh
+++ recovery/root/system/bin/detect-dynamic-partitions.sh
@@ -3,6 +3,7 @@
 if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep mi439_dynpart > /dev/null; then
     # Dynamic partitions
     cat /system/etc/recovery.fstab.dynamic >> /system/etc/recovery.fstab
+    cat /system/etc/twrp.flags.dynamic >> /system/etc/twrp.flags
 else
     # Non Dynamic partitions
     cat /system/etc/twrp.flags.nondynamic >> /system/etc/twrp.flags
