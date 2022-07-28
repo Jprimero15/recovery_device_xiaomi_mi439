091a39218808466b855771acf91d1e3e3b33b5a2
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-28 10:17:37 +0800

    mi439:  Set device codename/model props depending on the device
    [some edits]
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

---
+++ recovery/root/system/bin/set-device-props.sh
@@ -0,0 +1,30 @@
+#!/sbin/sh
+
+set_device_codename() {
+	resetprop "ro.build.product" "$1"
+	resetprop "ro.product.device" "$1"
+	for i in odm product system system_ext vendor; do
+		resetprop "ro.product.${i}.device" "$1"
+	done
+}
+
+set_device_model() {
+	resetprop "ro.product.model" "$1"
+	for i in odm product system system_ext vendor; do
+		resetprop "ro.product.${i}.model" "$1"
+	done
+}
+
+case "$(cat /sys/firmware/devicetree/base/model)" in
+	"PINE QRD")
+		set_device_codename "pine"
+		set_device_model "Redmi 7A"
+		;;
+	"Olive QRD")
+		set_device_codename "olives"
+		set_device_model "Redmi 8"
+		setprop "persist.vendor.ctm.disallowed" "true"
+		;;
+esac
+
+exit 0
