2bb026cde56654e84fc3fe9816a9fa7c2da79b80
Jprimero15 <jprimero155@gmail.com>
2022-07-29 16:23:42 +0800

    mi439: simplify dynamic partition fstab changes
    * also just make a separate fstabs for non dynamic and dynamic and update it while at it.
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- recovery/root/system/bin/detect-dynamic-partitions.sh
+++ recovery/root/system/bin/detect-dynamic-partitions.sh
@@ -2,9 +2,13 @@
 
 if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep mi439_dynpart > /dev/null; then
     # Dynamic partitions
-    cat /system/etc/recovery.fstab.dynamic >> /system/etc/recovery.fstab
-    cat /system/etc/twrp.flags.dynamic >> /system/etc/twrp.flags
+    echo "I:OrangeFoxLOLZ: Dynamic Partitions Detected" >> /tmp/recovery.log
 else
     # Non Dynamic partitions
-    cat /system/etc/twrp.flags.nondynamic >> /system/etc/twrp.flags
-fi
+    echo "I:OrangeFoxLOLZ: Normal Partitions Detected" >> /tmp/recovery.log
+    echo "I:OrangeFoxLOLZ: Replacing fstabs now.." >> /tmp/recovery.log
+    cp -fr /system/etc/recovery.fstab.nondynamic /system/etc/recovery.fstab
+    cp -fr /system/etc/twrp.flags.nondynamic /system/etc/twrp.flags
+    chmod 644 /system/etc/recovery.fstab
+    chmod 644 /system/etc/twrp.flags
+fi;
