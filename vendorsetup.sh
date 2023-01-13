5b5d56ddc6e11e97a389a9091402f18e54b9fd1d
Jprimero15 <jprimero155@gmail.com>
2023-01-13 19:13:57 +0800

    vendorsetup.sh: Update and Cleanups
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #
 # This file is part of the OrangeFox Recovery Project
-# Copyright (C) 2022 The OrangeFox Recovery Project
+# Copyright (C) 2023 The OrangeFox Recovery Project
 #
 # OrangeFox is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,29 +22,29 @@ export TW_DEFAULT_LANGUAGE="en"
 export LC_ALL="C"
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export TARGET_DEVICE_ALT="mi439,olive,olives,olivelite,olivewood,pine"
+export TARGET_DEVICE_ALT="Mi439,mi439,olive,olives,olivelite,olivewood,pine"
 export OF_KEEP_FORCED_ENCRYPTION=1
-export OF_USE_MAGISKBOOT=1
 export FOX_USE_LZMA_COMPRESSION=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800" # Tue Nov 14 15:20:00 GMT 2017
-export OF_NO_MIUI_PATCH_WARNING=1
-export OF_USE_GREEN_LED=0
-export FOX_USE_SED_BINARY=1
-export FOX_USE_XZ_UTILS=1
 export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
 export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-export OF_FBE_METADATA_MOUNT_IGNORE=1
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800" # Tue Nov 14 15:20:00 GMT 2017
+export OF_NO_MIUI_PATCH_WARNING=1
 export OF_NO_SAMSUNG_SPECIAL=1
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_USE_GREEN_LED=0
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
 export OF_HIDE_NOTCH=1
 export OF_CLOCK_POS=1
-export OF_ALLOW_DISABLE_NAVBAR=0
-export OF_QUICK_BACKUP_LIST="/boot;/data;"
+export OF_QUICK_BACKUP_LIST="/boot;/dtbo;"
 export FOX_VERSION="R11.1_4"
-export FOX_VARIANT="A12L"
+export FOX_BUILD_TYPE="STABLE"
+export FOX_VARIANT="A12"
 export OF_MAINTAINER="Jprimero15"
-export OF_MAINTAINER_AVATAR="/root/mzic/Jprimero15.png"
-export FOX_USE_SPECIFIC_MAGISK_ZIP="/root/mzic/Magisk-v25.2.zip"
